2d11c8057e711c5df568c2bf5e42a1e249d17649
GiulianoCretella <giulianocretella.1@gmail.com>
2022-06-13 15:53:02 +0200

    scss modifiche

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -8,7 +8,7 @@
     <link rel="stylesheet" href="{{asset('css/app.css')}}">
 </head>
 <body>
-    
+    <h1>BoolMovieDbQuery</h1>
     <div class="container">
         <div class="row">
             @foreach ($movies as $movie)
