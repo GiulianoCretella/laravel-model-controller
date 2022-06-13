5575932edb7e7cccd09ca5bd3ff511177930be3d
GiulianoCretella <giulianocretella.1@gmail.com>
2022-06-13 15:36:21 +0200

    first commit

---
+++ resources/views/home.blade.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+    <link rel="stylesheet" href="{{asset('css/app.css')}}">
+</head>
+<body>
+    
+    <div class="container">
+        <div class="row">
+            @foreach ($movies as $movie)
+            <div class="col-6">
+                <div class="card">
+                    <h4><a href="route('details',$movie->id)">Titolo: {{$movie->title}}</a></h4>
+                    <p>Titolo Originale: {{$movie->original_title}}</p>
+                    <p>Nazionalità: {{$movie->nationality}}</p>
+                    <p>Voto: {{$movie->vote}}</p>
+                    <p>Data di uscita: {{$movie->date}}</p>           
+                </div>  
+            </div>
+            @endforeach
+        </div> 
+    </div>
+</body>
+</html>
